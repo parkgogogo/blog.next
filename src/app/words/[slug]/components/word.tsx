af1c37ad9827834762f23e3e7d1e403db7497024
gyuannn <gyuannn1100@gmail.com>
2025-08-15 23:14:43 +0800

    perf: 缓存音频，避免重复请求

--- src/app/words/[slug]/components/word.tsx
+++ src/app/words/[slug]/components/word.tsx
@@ -1,32 +1,31 @@
 "use client";
 
 import { generateSpeech } from "@/app/words/[slug]/actions";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Loader } from "lucide-react";
 
 export const Word: React.FC<{ text: string }> = ({ text }) => {
   const [loading, setLoading] = useState(false);
+  const audioUrlRef = useRef<string | null>(null);
 
   const handlePlay = async () => {
-    setLoading(true);
     try {
-      const base64Str = await generateSpeech(text);
-      const byteCharacters = atob(base64Str);
-      const byteArray = new Uint8Array(byteCharacters.length);
-      for (let i = 0; i < byteCharacters.length; i++) {
-        byteArray[i] = byteCharacters.charCodeAt(i);
+      if (!audioUrlRef.current) {
+        setLoading(true);
+        const base64Str = await generateSpeech(text);
+        const byteCharacters = atob(base64Str);
+        const byteArray = new Uint8Array(byteCharacters.length);
+        for (let i = 0; i < byteCharacters.length; i++) {
+          byteArray[i] = byteCharacters.charCodeAt(i);
+        }
+
+        const blob = new Blob([byteArray], { type: "audio/wav" });
+        audioUrlRef.current = URL.createObjectURL(blob);
       }
 
-      const blob = new Blob([byteArray], { type: "audio/wav" });
-      const url = URL.createObjectURL(blob);
-
       const audio = new Audio();
-      audio.src = url;
+      audio.src = audioUrlRef.current;
       await audio.play();
-
-      audio.onended = () => {
-        URL.revokeObjectURL(url);
-      };
     } finally {
       setLoading(false);
     }
@@ -34,14 +33,17 @@ export const Word: React.FC<{ text: string }> = ({ text }) => {
 
   return (
     <div>
-      <h3 onClick={handlePlay} className="inline-block">
-        {text}
+      <h3 onClick={handlePlay}>
+        <span>{text}</span>
+        {loading && (
+          <span className="ml-1">
+            <Loader
+              className="text-blue-500 inline-block leading-6 pb-0.5"
+              size={16}
+            />
+          </span>
+        )}
       </h3>
-      {loading && (
-        <div className="ml-2 inline-block">
-          <Loader className="animate-spin text-blue-500" size={16} />
-        </div>
-      )}
     </div>
   );
 };
